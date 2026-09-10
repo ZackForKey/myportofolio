18bebd6ff616fef968d8448b411506345c70e7db
ZackForKey <zakyprastio07@gmail.com>
2026-09-10 17:27:10 +0700

    adding projects urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, show_projects
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('projects/',show_projects,name='show_projects'),
 ]
