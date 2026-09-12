dd6bcde8b5cbf383b777cf859a07cf469ca8b875
ZackForKey <zakyprastio07@gmail.com>
2026-09-12 23:24:40 +0700

    adding experience And Css

--- portofolio_main/admin.py
+++ portofolio_main/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Experience  # Sesuaikan nama model jika berbeda (misal: PortfolioExperience)
 
-# Register your models here.
+admin.site.register(Experience)
